1ed3184dc176086344094050cf1b5ec8ba32e2d3
RMcTn <rmctiernan@protonmail.com>
2024-08-18 10:36:59 +0100

    Add drawing with color change test

---
+++ run-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cargo run test --replay-path tests/draw_with_color_change/draw_with_color_change.rae --save-path draw_with_color-TEST.sav --save-after-replay --quit-after-replay
+diff_result=$(diff tests/draw_with_color_change/draw_with_color_change.sav draw_with_color-TEST.sav)
+
+
+if [ -z $diff_result ]; then
+	echo "PASS"
+	exit 0
+else
+	echo "FAIL - $diff_result"
+	exit 1
+fi
+
