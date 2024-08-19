e569e1398df07d6b27f0a86f2164e7a108e9482f
RMcTn <rmctiernan@protonmail.com>
2024-08-19 17:52:38 +0100

    Try a bit of x11 fixing

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export DISPLAY=:0.0
 cargo run test --replay-path tests/draw_with_color_change/draw_with_color_change.rae --save-path draw_with_color-TEST.sav --save-after-replay --quit-after-replay
 diff_result=$(diff tests/draw_with_color_change/draw_with_color_change.sav draw_with_color-TEST.sav)
 
